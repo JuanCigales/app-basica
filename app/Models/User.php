87e32bc194a8ea71750ca204b8a81e52d69e834f
Juan Cigales <jcigales@192.168.1.121>
2024-07-16 10:33:18 +0200

    index y create bien

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function resumes() {
+        return $this->hasMany(Resume::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ResumeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +11,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/resumes/create', [ResumeController::class, 'create'])->name('resumes.create');
+Route::get('/resumes/{resume}', [ResumeController::class, 'show'])->name('resumes.show');
+Route::put('/resumes/{resume}', [ResumeController::class, 'update'])->name('resumes.update');
+Route::delete('/resumes/{resume}', [ResumeController::class, 'destroy'])->name('resumes.destroy');
+Route::get('/resumes/{resume}/edit', [ResumeController::class, 'edit'])->name('resumes.edit');
+Route::get('/resumes', [ResumeController::class, 'index'])->name('resumes.index');
+Route::post('/resumes', [ResumeController::class, 'store'])->name('resumes.store');
